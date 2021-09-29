7d17ed8c22936713975715a50bc385f6c0578b91
Luka <lukagrujic80@gmail.com>
2021-09-29 11:32:20 +0200

    get one project

--- src/backend/project/controller.ts
+++ src/backend/project/controller.ts
@@ -7,8 +7,10 @@ import {
   JWTPermissionName,
   JWTRoleName,
 } from '@becomes/purple-cheetah-mod-jwt/types';
+import { HTTPStatus } from '@becomes/purple-cheetah/types';
+import { ProjectFactory } from '.';
 import { Repo } from '../repo';
-import { ProjectFactory } from './factory';
+import type { ProjectProtected } from '../types';
 import { createProjectRepo } from './repository';
 
 export const ProjectController = createController({
@@ -19,7 +21,7 @@ export const ProjectController = createController({
   },
   methods() {
     return {
-      getAll: createControllerMethod({
+      getAll: createControllerMethod<unknown, ProjectProtected[]>({
         path: '/all',
         type: 'get',
         preRequestHandler: createJwtProtectionPreRequestHandler(
@@ -29,6 +31,24 @@ export const ProjectController = createController({
         async handler() {
           const projects = await Repo.project.findAll();
           return projects.map((e) => ProjectFactory.toProtected(e));
+        },
+      }),
+      getOne: createControllerMethod<unknown, ProjectProtected>({
+        path: '/:id',
+        type: 'get',
+        preRequestHandler: createJwtProtectionPreRequestHandler(
+          [JWTRoleName.ADMIN, JWTRoleName.USER],
+          JWTPermissionName.READ
+        ),
+        async handler({ request, errorHandler }) {
+          const project = await Repo.project.findById(request.params.id);
+          if (!project) {
+            throw errorHandler.occurred(
+              HTTPStatus.NOT_FOUNT,
+              `Project with ID "${request.params.id}" does not exist.`
+            );
+          }
+          return ProjectFactory.toProtected(project);
         },
       }),
     };
