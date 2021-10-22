9a607ae15f14eaff1dbb5ae2b05a1c4cc58e72a0
Bbanez <banezprod@gmail.com>
2021-10-22 13:24:09 +0200

    Improve bundle

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,17 +1,10 @@
 import { createBcmsPlugin } from '@becomes/cms-backend/plugin';
-import { BCMSConfig } from '@becomes/cms-backend/config';
-import { ProjectController } from './project';
-import { SwaggerController, SwaggerMiddleware } from './swagger';
 import type { Controller, Middleware } from '@becomes/purple-cheetah/types';
-import { JobController } from './job/controller';
+import { ProjectController } from './project';
+import { JobController } from './job';
 
 const controllers: Controller[] = [ProjectController, JobController];
 const middleware: Middleware[] = [];
-
-if (BCMSConfig.local) {
-  controllers.push(SwaggerController);
-  middleware.push(SwaggerMiddleware);
-}
 
 export default createBcmsPlugin({
   name: 'bcms-plugin---name',
